577d85cc8bc03c3fcd1e5d9945b3c1b0ebaf5cb9
Jia Xu <dolremi@gmail.com>
2016-07-17 23:11:32 -0500

    Add main function for median calculator

--- Algorithm/Heap/MedianCalculator.py
+++ Algorithm/Heap/MedianCalculator.py
@@ -26,4 +26,13 @@ class MedianCalculator:
             self.median = self.lower_half[0]
         else:
             self.median = (self.lower_half[0] - self.upper_half[0])/2.0
+        return self.median
+
+
+if __name__ == "__main__":
+    track = MedianCalculator()
+    sample = [15, 9, 25, -34, 45, 56]
+    for i in sample:
+        track.insert(i)
+        print("The median of the list is {0}".format(track.get_median()))
 

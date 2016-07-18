05bdb80205281f8bc8737c9908eb66de896e323d
Jia Xu <dolremi@gmail.com>
2016-07-17 19:50:56 -0500

    Add simple heap implementation

---
+++ Algorithm/Heap/MedianCalculator.py
@@ -0,0 +1,29 @@
+from __future__ import division, absolute_import, print_function
+import heapq
+
+
+class MedianCalculator:
+    """This class dynamically track down the median of the streaming of data,
+       it consists of two heaps: one min-heap called upper_half and max-heap called lower_half,
+       keep the number of lower_half at most one element more than upper_half
+       and the elements in lower_half is no larger than that in upper_half
+       the max heap can be implemented as heapq with inverted values"""
+    def __init__(self):
+        self.upper_half = []
+        self.lower_half = []
+        self.median = 0
+
+    def insert(self, element):
+        if len(self.upper_half) == len(self.lower_half):
+            heapq.heappush(self.lower_half, -heapq.heappushpop(self.upper_half, -element))
+        else:
+            heapq.heappush(self.upper_half, -heapq.heappushpop(self.lower_half, element))
+
+    def get_median(self):
+        upper_len = len(self.upper_half)
+        lower_len = len(self.lower_half)
+        if lower_len > upper_len:
+            self.median = self.lower_half[0]
+        else:
+            self.median = (self.lower_half[0] - self.upper_half[0])/2.0
+
---
+++ Algorithm/Heap/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import division, absolute_import, print_function
+
+_all__= ["maxheap", "MedianCalculator"]
